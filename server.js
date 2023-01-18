577611db593102cef60a9f10ddc48a2bd464ef23
Harshad.N <harshad.n4321@gmail.com>
2023-01-18 09:46:42 +0530

    updated the app layout

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-const express = require('express'); 
+const express = require('express');
 const app = express()
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
+const path = require('path');
 
 
-const PORT = process.env.PORT|| 3000;
+
+var engines = require('consolidate');
+var swig = require('swig')
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.engine('html', engines.swig);
+app.set('views', __dirname + '/layout');
+app.set('view engine', 'html');
+
+const usersRoute = require('./routers/index.js')
+
+app.use(express.static(__dirname + '/public'))
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + 'index')
+// })
+
+
+app.use('/', usersRoute)
+
 // server.listen(3000);
-server.listen(PORT,()=>{
-    console .log(`server is connected to port ${(PORT)}.`);
-})  
-
-app.use(express.static(__dirname+'/public'))
- 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
+server.listen(PORT, () => {
+    console.log(`server is connected to port ${(PORT)}.`);
 })
 //socket .io
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('connected,...');
-    socket.on('message',(msg)=>{
+    socket.on('message', (msg) => {
         console.log(msg)
-        socket.broadcast.emit('message',msg)
+        socket.broadcast.emit('message', msg)
     })
 })
